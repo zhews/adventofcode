b9433d2ecd71bee49a26c8e270981dd6701d28f8
zhews <118117386+zhews@users.noreply.github.com>
2022-12-01 23:18:54 +0100

    revert: remove some debug code

--- rust/2022/01/src/main.rs
+++ rust/2022/01/src/main.rs
@@ -9,22 +9,6 @@ const INPUT_FILE_NAME: &str = "input.txt";
 
 fn main() {
     let input = std::fs::read_to_string(INPUT_FILE_NAME).expect("No input file found!");
-    let input_2 = "1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000";
-    let elves: Vec<&str> = input_2.split("\n\n").collect();
-    println!("{}", elves.join(" - "));
     println!("Part01: {}", part_01(&input));
     println!("Part02: {}", part_02(&input));
 }
