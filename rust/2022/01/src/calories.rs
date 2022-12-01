30a77a1075f5299ee92ac3f96d0b084c1aab3975
zhews <118117386+zhews@users.noreply.github.com>
2022-12-01 22:46:28 +0100

    test(rust): add test for compute_calorie_totals function for day 1 of 2022

--- rust/2022/01/src/calories.rs
+++ rust/2022/01/src/calories.rs
@@ -1,7 +1,10 @@
 pub fn compute_calorie_totals(input: &str) -> Vec<i32> {
     let mut calorie_total = 0;
     let mut calorie_totals = Vec::new();
-    for line in input.lines() {
+    let lines = input.lines();
+    let last_index = lines.count();
+    let lines = input.lines();
+    for (index, line) in lines.enumerate() {
         if line.is_empty() {
             calorie_totals.push(calorie_total);
             calorie_total = 0;
@@ -9,6 +12,40 @@ pub fn compute_calorie_totals(input: &str) -> Vec<i32> {
         }
         let calories: i32 = line.parse().expect("Invalid");
         calorie_total += calories;
+        if index + 1 == last_index {
+            calorie_totals.push(calorie_total);
+        }
     }
     calorie_totals
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn empty_input() {
+        let calorie_totals = compute_calorie_totals("");
+        assert_eq!(calorie_totals.len(), 0)
+    }
+
+    #[test]
+    fn example_input() {
+        let calorie_totals = compute_calorie_totals(r#"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"#);
+        let expected = vec![6000, 4000, 11000, 24000, 10000];
+        assert_eq!(calorie_totals, expected)
+    }
 }
