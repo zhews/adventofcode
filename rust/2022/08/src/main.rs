40f36f07524cec4004874a220bc143969c92c6e7
zhews <118117386+zhews@users.noreply.github.com>
2022-12-09 09:25:45 +0100

    feat(rust): add part 2 for day 8 of 2022

--- rust/2022/08/src/main.rs
+++ rust/2022/08/src/main.rs
@@ -1,11 +1,14 @@
 use part_01::part_01;
+use crate::part_02::part_02;
 
 mod parser;
 mod part_01;
+mod part_02;
 
 const INPUT_FILE_NAME: &str = "input.txt";
 
 fn main() {
     let input = std::fs::read_to_string(INPUT_FILE_NAME).unwrap();
-    println!("{}", part_01(&input))
+    println!("{}", part_01(&input));
+    println!("{}", part_02(&input));
 }
---
+++ rust/2022/08/src/part_02.rs
@@ -0,0 +1,48 @@
+use crate::parser::parse_map;
+
+pub fn part_02(input: &str) -> usize {
+    let map = parse_map(&input);
+    let mut scenic_scores = Vec::new();
+    for (row_index, row) in map.iter().enumerate() {
+        for (tree_index, tree) in row.iter().enumerate() {
+            let mut left = 0;
+            let mut right = 0;
+            let mut up = 0;
+            let mut down = 0;
+            // to left
+            for tree_index_left in (0..tree_index).rev() {
+                let tree_left = row.get(tree_index_left).unwrap();
+                left += 1;
+                if tree_left >= tree {
+                    break
+                }
+            }
+            // to right
+            for tree_index_right in tree_index+1..row.len() {
+                let tree_right = row.get(tree_index_right).unwrap();
+                right += 1;
+                if tree_right >= tree {
+                    break
+                }
+            }
+            // up
+            for tree_index_up in (0..row_index).rev() {
+                let tree_up = map.get(tree_index_up).unwrap().get(tree_index).unwrap();
+                up += 1;
+                if tree_up >= tree {
+                    break
+                }
+            }
+            // down
+            for tree_index_down in row_index+1..map.len() {
+                let tree_down = map.get(tree_index_down).unwrap().get(tree_index).unwrap();
+                down += 1;
+                if tree_down >= tree {
+                    break
+                }
+            }
+            scenic_scores.push(left * right * up * down);
+        }
+    }
+    *scenic_scores.iter().max().unwrap() as usize
+}
